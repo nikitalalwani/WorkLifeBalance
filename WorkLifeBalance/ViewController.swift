c36cfd662a32297b52d2eaf5e496b3eee8f5e73d
nikita lalwani <nikitalalwani@nikitas-macbook-air.local>
2019-04-11 15:22:58 -0400

    Initial Commit

---
+++ WorkLifeBalance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkLifeBalance
+//
+//  Created by nikita lalwani on 4/11/19.
+//  Copyright © 2019 nikita lalwani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
